326cca5ef9f4b173fa55a4f3ce8cb969ae84a795
Daniel Samek <samek@spstrutnov.cz>
2022-03-21 11:30:34 +0100

    kosik

--- eshop/pages/index.js
+++ eshop/pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
           Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla quis diam. Nunc tincidunt ante vitae massa. Fusce wisi. Aenean placerat. Pellentesque arcu. Nulla pulvinar eleifend sem. Proin mattis lacinia justo. Maecenas lorem. Integer malesuada. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Curabitur sagittis hendrerit ante. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Vivamus porttitor turpis ac leo. Vivamus ac leo pretium faucibus. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Suspendisse sagittis ultrices augue. Pellentesque pretium lectus id turpis. Etiam commodo dui eget wisi.
         </div>
         <div className="Products">
-        {data.data.map((item) =>  (
+        {data.data.map((item) => (
           <div className="product">
           <p>{item.code}</p>
           <p>{item.availability}</p>
--- eshop/pages/kosik.js
+++ eshop/pages/kosik.js
@@ -3,20 +3,32 @@ import { useState } from "react";
 import Header from "../components/header"
 export default function kostatko(){
 
-    let [data, getdata] = useState("0")
-
-
+    let [data, getdata] = useState([]);
     useEffect(() =>{
-        getdata(localStorage.getItem("products"))
-    })
+        getdata(JSON.parse(localStorage.getItem("products")));
+    }, [])
+    if(!data){
+        return(Header())
+    }
+    
+    function calculate(count, ){
+
+    }
 
     return(
         <div>
         {Header()}
         <h1>Košík</h1>
-        {data.map((item) =>{
-            <p>{item}</p>
-        })}
+        {data.map((item) => (
+            <div>
+            <img src={item.prod.imagename}></img>
+            <p>Název: {item.prod.name}</p>            
+            <p>Kód: {item.prod.code}</p>
+            <p>Dostupnost: {item.prod.availability}</p>
+            <p>Jednotková cena: {item.prod.price}</p>
+            <p>Celková cena: {calculate()}</p>
+            </div>
+        ))}
         </div>
     )
 
